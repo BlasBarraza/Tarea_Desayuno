00ed58d69b16e00b01d221f0e52f01b9b15f1b7d
BlasBarraza <blas.barrazav@utem.cl>
2018-12-14 23:10:45 -0300

    Desayuno Listo

---
+++ DesayunoDefinitivo.R
@@ -0,0 +1,28 @@
+#Elementos para Realizar desayuno
+#"Pan"
+#"Palta"
+#"Naranjas"
+#"Cafe"
+#"Harina"
+#"Huevos"
+
+#Primer paso; cortar los elementos:
+Cortar ( "pan" )
+Cortar ( "palta" )
+Cortar ( "naranjas" )
+
+#Segundo paso; preparar, calentar y moler elementos segun corresponda:
+Calentar ( "cafe" )
+Calentar ( "pan cortado" )
+Moler ( "palta cortada" )
+Preparar ( "naranjas cortadas" )
+Preparar ( "harina" , "huevos" )
+
+#Tercer paso; terminar de preparar los "sub-elementos":
+CalentarSarten ()
+Preparar ( "masa de panqueques","sartÃ©n caliente" )
+Preparar ( "pan tostado" , "palta molida" )
+Rellenar ( "panqueques" )
+
+#Cuarto y ultimo paso; servir.
+Servir("pan tostado con palta", "panqueques con manjar", "jugo de naranjas", "cafe caliente")
